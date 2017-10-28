48eeb52df05d5fd29a1d18b6e1f9b08ce8d9e164
Simone Cesano <simonecesano@libero.it>
2017-10-28 23:19:21 +0200

    added documentation; setDataFromURL can now omit callback

--- static/storage.js
+++ static/storage.js
@@ -1,6 +1,6 @@
 Storage.prototype.getData = function(id){
     var d = this.getItem(id)
-    if (d) { return JSON.parse(d) } else { return {} };
+    if (d) { return JSON.parse(d) } else { return undefined };
 }
 
 Storage.prototype.setData = function(id, data, merge){
@@ -12,13 +12,34 @@ Storage.prototype.setData = function(id, data, merge){
 
 Storage.prototype.setDataFromURL = function(url, id, callback, merge){
     var s = this;
-    // console.log(merge);
+
+    if (arguments[2] instanceof Function) { callback = arguments[2] }
+    if (typeof arguments[2] === 'boolean')  { merge = arguments[2]; callback = undefined }
+    if (typeof arguments[3] === 'boolean')  { merge = arguments[3] }
+    
     if (callback && this.getItem(id)) { callback(this.getData(id)) }
+    
     return $.get(url, function(d){
 	s.setData(id, d, merge)
 	if (callback) { callback(d) }
     })
 };
 
-// mergeData
-// expires
+
+/****************************************************************************************************
+
+    getData(id)
+
+gets data from storage and parses it via JSON
+
+    setData(id, data[, merge])
+
+sets data into storage after encoding it it as JSON. If 'merge' is true, it merges the data with 
+existing stored data.
+
+    setDataFromURL(url, id[, callback][, merge])
+
+sets data into storage from data retrieved from 'url'. Executes 'callback' immediately, and again
+after retrieving the remote data. If 'merge'  is true, it merges the data with existing stored data.
+
+*****************************************************************************************************/
